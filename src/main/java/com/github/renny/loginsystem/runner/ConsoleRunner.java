9c81a12d6e336e28b13abe72e224e52b7c472ecd
renny <meow3199@gmail.com>
2026-03-02 14:44:24 +0800

    Refactor ConsoleRunner

--- src/main/java/com/github/renny/loginsystem/runner/ConsoleRunner.java
+++ src/main/java/com/github/renny/loginsystem/runner/ConsoleRunner.java
@@ -39,16 +39,16 @@ public class ConsoleRunner implements CommandLineRunner {
             String num = scan.nextLine();
             switch (num){
                 case "1":
-                    register(scan,authService);
+                    register(scan);
                     break;
                 case "2":
-                    login(scan,session,authService);
+                    login(scan);
                     break;
                 case "3":
-                    showCurrentUser(session);
+                    showCurrentUser();
                     break;
                 case "4":
-                    logout(session);
+                    logout();
                     break;
                 case "0":
                     System.out.println("謝謝使用本系統。");
@@ -64,69 +64,69 @@ public class ConsoleRunner implements CommandLineRunner {
 
 
 //註冊
-private void register(Scanner scan, AuthService authService){
-    try{
-        System.out.println("帳號設置規則:帳號長度為6-20字,只能包含英文字母與數字,且不得使用數字做開頭。");
-        System.out.print("請輸入帳號:");
-        String account = scan.nextLine();
+    private void register(Scanner scan){
+        try{
+            System.out.println("帳號設置規則:帳號長度為6-20字,只能包含英文字母與數字,且不得使用數字做開頭。");
+            System.out.print("請輸入帳號:");
+            String account = scan.nextLine();
 
-        System.out.println("密碼設置規則:密碼長度8-16位數，且需包含至少1個英文大寫,1個英文小寫及數字組成。");
-        System.out.print("請輸入密碼:");
-        String password = scan.nextLine();
+            System.out.println("密碼設置規則:密碼長度8-16位數，且需包含至少1個英文大寫,1個英文小寫及數字組成。");
+            System.out.print("請輸入密碼:");
+            String password = scan.nextLine();
 
-        System.out.print("請輸入使用者名稱:");
-        String userName = scan.nextLine();
+            System.out.print("請輸入使用者名稱:");
+            String userName = scan.nextLine();
 
-        authService.register(account,userName,password);
-        System.out.println("註冊成功。");
-    } catch (RuntimeException e) {
-        System.out.println("註冊失敗," + e.getMessage());
+            authService.register(account,userName,password);
+            System.out.println("註冊成功。");
+        } catch (RuntimeException e) {
+            System.out.println("註冊失敗," + e.getMessage());
+        }
+
     }
-
-}
 
 //登入
-private void login(Scanner scan, LoginSession session, AuthService authService){
-    try{
-        System.out.print("請輸入帳號:");
-        String account = scan.nextLine();
+    private void login(Scanner scan){
+        try{
+            System.out.print("請輸入帳號:");
+            String account = scan.nextLine();
 
-        System.out.print("請輸入密碼:");
-        String password = scan.nextLine();
+            System.out.print("請輸入密碼:");
+            String password = scan.nextLine();
 
-        User user = authService.login(account,password);
-        session.login(user);
-        System.out.println("登入成功!歡迎回來," + user.getUserName());
-    }catch (AccountNotFoundException e){
-        System.out.println(e.getMessage());
-    }catch (AccountLockedException e){
-        System.out.println(e.getMessage());
-    }catch (PasswordMismatchException e){
-        System.out.println(e.getMessage());
+            User user = authService.login(account,password);
+            session.login(user);
+            System.out.println("登入成功!歡迎回來," + user.getUserName());
+        }catch (AccountNotFoundException e){
+            System.out.println(e.getMessage());
+        }catch (AccountLockedException e){
+            System.out.println(e.getMessage());
+        }catch (PasswordMismatchException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 
-}
-
-//顯示目前登入身分
-private void showCurrentUser(LoginSession session){
-    try{
-        User user = session.getCurrentUser();
-        System.out.println("目前使用者為:" + user.getUserName());
-    }catch (IllegalStateException e){
-        System.out.println("錯誤," + e.getMessage());
-    }
-}
-
-//登出
-private void logout(LoginSession session){
-    if(session.isLoggedIn()){
-        session.logout();
-        System.out.println("登出成功!");
-    }else{
-        System.out.println("尚未登入。");
+    //顯示目前登入身分
+    private void showCurrentUser(){
+        try{
+            User user = session.getCurrentUser();
+            System.out.println("目前使用者為:" + user.getUserName());
+        }catch (IllegalStateException e){
+            System.out.println("錯誤," + e.getMessage());
+        }
     }
 
-}
+    //登出
+    private void logout(){
+        if(session.isLoggedIn()){
+            session.logout();
+            System.out.println("登出成功!");
+        }else{
+            System.out.println("尚未登入。");
+        }
+
+    }
 
 }
 
